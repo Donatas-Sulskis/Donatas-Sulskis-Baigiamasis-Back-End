71c5323d9a30394b2632bb45a789500b04d5205b
Donatas <donatas.sul@gmail.com>
2021-07-24 13:23:11 +0300

    necessary imports

--- src/routes/content.js
+++ src/routes/content.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const middleware = require("../middleware");
+const mysql = require("mysql2/promise");
+
+const { mysqlConfig } = require("../config");
 
 module.exports = router;
