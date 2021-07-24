1edb43b4a27f6cba0da1eab3ee51068c3f76d538
Donatas <donatas.sul@gmail.com>
2021-07-24 13:45:04 +0300

    added get for all sugars

--- src/routes/content.js
+++ src/routes/content.js
@@ -5,4 +5,22 @@ const mysql = require("mysql2/promise");
 
 const { mysqlConfig } = require("../config");
 
+router.get("/dashboard", middleware.loggedIn, async (req, res) => {
+  try {
+    const con = await mysql.createConnection(mysqlConfig);
+    const [data] = await con.execute(
+      `SELECT * FROM sugars 
+      WHERE user_id = ${req.userData.id}`
+    );
+
+    con.end();
+
+    return res.status(200).send(data);
+  } catch (e) {
+    console.log(e);
+
+    return res.status(500).send({ error: "DB error" });
+  }
+});
+
 module.exports = router;
